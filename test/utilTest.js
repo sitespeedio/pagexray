7950d96e52173c6a5ebcc23a890a537d35853472
Tobias Lidskog <tobias@lidskog.se>
2015-09-18 16:17:42 +0200

    Generate getContentType tests from examples.

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,103 +1,31 @@
 'use strict';
 
 var assert = require('chai').assert,
-  util = require('../lib/util');
+  util = require('../lib/util'),
+  forEach = require('lodash.foreach');
 
 describe('util', function() {
 
   describe('#getContentType', function() {
+    var typeToMimeMapping = {
+      'doc': ['text/plain', 'text/html', 'text/html; charset=utf-8'],
+      'js': ['text/javascript', 'application/x-javascript; charset=utf-8'],
+      'css': ['text/css'],
+      'image': ['image/png', 'image/jpg', 'image/gif', 'image/x-icon', 'image/svg+xml', 'image/webp'],
+      'font': ['application/font-woff', 'application/font-sfnt', 'application/x-font-opentype',
+        'application/x-font-ttf'],
+      'flash': ['application/x-shockwave-flash'],
+      'unknown': ['application/my-own-type']
+    };
 
-    it('text/plain should be categorized as doc', function() {
-      var result = util.getContentType('text/plain');
-      assert.deepEqual(result, 'doc');
+    forEach(typeToMimeMapping, function(mimes, type) {
+      forEach(mimes, function(mime) {
+        it('should categorize ' + mime + ' as ' + type, function() {
+          var result = util.getContentType(mime);
+          assert.deepEqual(result, type);
+        });
+      });
     });
-
-    it('text/html should be categorized as doc', function() {
-      var result = util.getContentType('text/html');
-      assert.deepEqual(result, 'doc');
-    });
-
-    it('text/html; charset=utf-8 with charset should be categorized as doc', function() {
-      var result = util.getContentType('text/html; charset=utf-8');
-      assert.deepEqual(result, 'doc');
-    });
-
-    it('text/javascript should be categorized as js', function() {
-      var result = util.getContentType('text/javascript');
-      assert.deepEqual(result, 'js');
-    });
-
-    it('application/x-javascript; charset=utf-8 should be categorized as js', function() {
-      var result = util.getContentType('application/x-javascript; charset=utf-8');
-      assert.deepEqual(result, 'js');
-    });
-
-    it('text/css should be categorized as css', function() {
-      var result = util.getContentType('text/css');
-      assert.deepEqual(result, 'css');
-    });
-
-    it('image/png should be categorized as image', function() {
-      var result = util.getContentType('image/png');
-      assert.deepEqual(result, 'image');
-    });
-
-    it('image/jpg should be categorized as image', function() {
-      var result = util.getContentType('image/jpg');
-      assert.deepEqual(result, 'image');
-    });
-
-    it('image/gif should be categorized as image', function() {
-      var result = util.getContentType('image/gif');
-      assert.deepEqual(result, 'image');
-    });
-
-    it('image/x-icon should be categorized as image', function() {
-      var result = util.getContentType('image/x-icon');
-      assert.deepEqual(result, 'image');
-    });
-
-    it('image/svg+xml should be categorized as image', function() {
-      var result = util.getContentType('image/svg+xml');
-      assert.deepEqual(result, 'image');
-    });
-
-    it('image/webp should be categorized as image', function() {
-      var result = util.getContentType('image/webp');
-      assert.deepEqual(result, 'image');
-    });
-
-
-    it('application/font-woff should be categorized as font', function() {
-      var result = util.getContentType('application/font-woff');
-      assert.deepEqual(result, 'font');
-    });
-
-    it('application/font-sfnt should be categorized as font', function() {
-      var result = util.getContentType('application/font-sfnt');
-      assert.deepEqual(result, 'font');
-    });
-
-    it('application/x-font-opentype should be categorized as font', function() {
-      var result = util.getContentType('application/x-font-opentype');
-      assert.deepEqual(result, 'font');
-    });
-
-    it('application/x-font-ttf should be categorized as font', function() {
-      var result = util.getContentType('application/x-font-ttf');
-      assert.deepEqual(result, 'font');
-    });
-
-    it('application/x-shockwave-flash should be categorized as flash', function() {
-      var result = util.getContentType('application/x-shockwave-flash');
-      assert.deepEqual(result, 'flash');
-    });
-
-    it('application/my-own-type should be categorized as unkown', function() {
-      var result = util.getContentType('application/my-own-type');
-      assert.deepEqual(result, 'unknown');
-    });
-
   });
 
   describe('#flattenHeaders', function() {

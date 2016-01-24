238465964761622a4a63a912f2d56ce019d8369f
soulgalore <peter@soulgalore.com>
2016-01-24 20:52:26 +0100

    verify the final URL

--- test/redirectTest.js
+++ test/redirectTest.js
@@ -12,6 +12,12 @@ describe('Redirect', function() {
     });
   });
 
+  it('The final URL should be right', function() {
+    return har.getPages('test/files/redirect/www.sitespeed.io-redirecting-to-https.har').then((result) => {
+      assert.strictEqual(result[0].finalUrl, 'https://www.sitespeed.io/');
+    });
+  });
+
   it('We should be able to identify frontend redirects or at least maybe guess them?');
 
 

6da0bfdd56040db0b158c60f31a3271ac834345c
soulgalore <peter@soulgalore.com>
2016-01-25 22:58:50 +0100

    test that we count number of content by type #1

---
+++ test/contentTypesTest.js
@@ -0,0 +1,18 @@
+
+'use strict';
+let assert = require('assert');
+let har = require('./helpers/har');
+
+
+describe('Check content types', function() {
+  it('We should be able to identify the number of content types', function() {
+    return har.getPages('test/files/contentTypes/linkedin.har').then((result) => {
+      console.log(result[0]);
+      assert.strictEqual(result[0].doc.requests, 1, 'We couldnt get the right number of docs');
+      assert.strictEqual(result[0].js.requests, 8, 'We couldnt get the right number of js');
+      assert.strictEqual(result[0].css.requests, 2, 'We couldnt get the right number of css');
+      assert.strictEqual(result[0].image.requests, 5, 'We couldnt get the right number of image');
+      assert.strictEqual(result[0].unknown.requests, 2, 'We couldnt get the right number of unknown');
+    });
+  });
+});

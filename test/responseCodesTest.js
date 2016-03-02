e121d898e8089b11a04d69762ca633000e53939a
soulgalore <peter@soulgalore.com>
2016-03-02 10:01:55 +0100

    bug fix: pick all stats per page instead of all pages

--- test/responseCodesTest.js
+++ test/responseCodesTest.js
@@ -7,10 +7,10 @@ describe('Resonse codes', function() {
 
   it('We should be able identify all response codes', function() {
     return har.getPages('test/files/redirect/aftonbladet.se-redirecting-to-www.har').then((result) => {
-      assert.strictEqual(result[0].responseCodes[200], 199, '200');
+      assert.strictEqual(result[0].responseCodes[200], 168, '200');
       assert.strictEqual(result[0].responseCodes[404], 1, '404');
-      assert.strictEqual(result[0].responseCodes[301], 2, '301');
-      assert.strictEqual(result[0].responseCodes[204], 4, '204');
+      assert.strictEqual(result[0].responseCodes[301], 1, '301');
+      assert.strictEqual(result[0].responseCodes[204], 2, '204');
     });
   });
 

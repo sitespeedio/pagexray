42cf270529fcf5d229386b1ed607034d354f0a2f
soulgalore <peter@soulgalore.com>
2016-04-17 21:07:50 +0200

    new name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
-# CHANGELOG - snufkin
+# CHANGELOG - PageXray
+
+version 0.5.0 - 2016-04-17
+------------------------
+### Changed
+* New name new game. The project is now known as project PageXray.
 
 version 0.4.0 - 2016-04-12
 ------------------------
 ### Added
 * Collect the full redirect chain
-
 
 version 0.3.8 - 2016-03-02
 ------------------------
@@ -14,5 +18,4 @@ version 0.3.8 - 2016-03-02
 version 0.3.7 - 2016-03-02
 ------------------------
 ### Fixed
-* Create statistics per page instead of take stats for all pages. 
-
+* Create statistics per page instead of take stats for all pages.
--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var snufkin = require('../lib/index'),
+var pagexray = require('../lib/index'),
   fs = require('fs'),
   minimist = require('minimist');
 
@@ -12,13 +12,13 @@ var argv = minimist(process.argv.slice(2), {
 
 if (argv.help || !argv._[0]) {
   console.log('   Convert a HAR file to a (better) page summary.');
-  console.log('   Usage: snufkin [options] pathToHarFile\n');
+  console.log('   Usage: pagexray [options] pathToHarFile\n');
   console.log('   Options:');
   console.log('   --pretty              Pretty format the JSON [false]');
   console.log('   --includeAssets       Include info about every asset in the result [false]');
 } else {
   var har = JSON.parse(fs.readFileSync(argv._[0]));
-  var pages = snufkin.convert(har, argv);
+  var pages = pagexray.convert(har, argv);
   if (argv.pretty) {
     console.log(JSON.stringify(pages, null, '  '));
   } else {

7a6db30fe1c38f5f26e0e770a8e58e7e14e2850c
Peter Hedenskog <peter@soulgalore.com>
2017-07-16 20:51:01 +0200

    Use Browserify to build a in browser version (#34)
    * Remove fast-stats dependency and only use min/median/max values #31
    * use babel to target es5 in the browser

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-# Super simple release script for pagexray
+# Super simple release script for PageXray
 # Lets use it it for now and make it better over time :)
 # You need np for this to work
 # npm install --global np
 np $*
-
-PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')

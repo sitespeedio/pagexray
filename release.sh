df498dfd2a0d37e0e792ffab0f489b6e6348ab33
Jonathan Lee <jonjohn1232009@gmail.com>
2017-11-19 19:45:07 -0500

    Bumping node to 8.9 and adding package-lock.json

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Super simple release script for PageXray
-# Lets use it it for now and make it better over time :)
+# Lets use it for now and make it better over time :)
 # You need np for this to work
 # npm install --global np
 np $*

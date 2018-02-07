cdbd28da2281cfffc210f4fd46c5e03aff7c11f2
Peter Hedenskog <phedenskog@wikimedia.org>
2018-02-07 13:52:48 +0100

    cleanup

--- test/convertUrlsTest.js
+++ test/convertUrlsTest.js
@@ -3,10 +3,13 @@ let assert = require('assert'),
   pluck = require('lodash.pluck'),
   har = require('./helpers/har');
 
-describe('Handle URLs with different different cases', function() {
-  it('Should handle HTTPS and https', function() {
-    return har.pagesFromTestHar('case-sensitive-protocol.har').then(result => {
-      const exectedUrls = ['https://www.verizonwireless.com/devices'];
+describe('Converting URLs', function() {
+  it('Should convert urls', function() {
+    return har.pagesFromTestHar('www.nytimes.com.har').then(result => {
+      const exectedUrls = [
+        'http://www.nytimes.com/',
+        'http://www.nytimes.com/'
+      ];
       assert.deepEqual(pluck(result, 'url'), exectedUrls);
     });
   });

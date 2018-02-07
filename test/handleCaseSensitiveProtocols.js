290c4f44d0778b8c53b4acce117b77728e11ed90
Peter Hedenskog <phedenskog@wikimedia.org>
2018-02-07 13:49:42 +0100

    fix

---
+++ test/handleCaseSensitiveProtocols.js
@@ -0,0 +1,17 @@
+'use strict';
+let assert = require('assert'),
+  pluck = require('lodash.pluck'),
+  har = require('./helpers/har');
+
+
+describe('Converting URLs', function() {
+
+  it('Should convert urls', function() {
+    return har.pagesFromTestHar('www.nytimes.com.har')
+      .then((result) => {
+        const exectedUrls = ['http://www.nytimes.com/', 'http://www.nytimes.com/'];
+        assert.deepEqual(pluck(result, 'url'), exectedUrls);
+      });
+  });
+
+});

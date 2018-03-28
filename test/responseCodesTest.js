089c7f9a677f494d579ee738e69abd414554c3dc
Peter Hedenskog <phedenskog@wikimedia.org>
2018-03-28 10:06:28 +0200

    Include the size of 304 responses see https://github.com/sitespeedio/sitespeed.io/issues/1963

--- test/responseCodesTest.js
+++ test/responseCodesTest.js
@@ -2,12 +2,11 @@
 let assert = require('assert');
 let har = require('./helpers/har');
 
-
 describe('Resonse codes', function() {
-
   it('We should be able identify all response codes', function() {
-    return har.pagesFromTestHar('redirect/aftonbladet.se-redirecting-to-www.har')
-      .then((result) => {
+    return har
+      .pagesFromTestHar('redirect/aftonbladet.se-redirecting-to-www.har')
+      .then(result => {
         assert.strictEqual(result[0].responseCodes[200], 168, '200');
         assert.strictEqual(result[0].responseCodes[404], 1, '404');
         assert.strictEqual(result[0].responseCodes[301], 1, '301');
@@ -15,5 +14,10 @@ describe('Resonse codes', function() {
       });
   });
 
-
+  it('304 should be included in the page size', function() {
+    return har.pagesFromTestHar('responseCodes/304.har').then(result => {
+      console.log(result[0].contentTypes.css);
+      console.log(result[0].contentTypes.javascript);
+    });
+  });
 });
